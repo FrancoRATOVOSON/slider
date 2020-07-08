5a877739c4454378e37b6182084d4e80cd57a594
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2020-07-08 09:37:26 +0200

    ***

--- desktop/Out/try.js
+++ desktop/Out/try.js
@@ -1,6 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Listener_1 = require("./Server/Listener");
-var server = new Listener_1.HttpServer();
-server.listen();
-server.ServerEvent.on("server:received", (data) => console.log(data));

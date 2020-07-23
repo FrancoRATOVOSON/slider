0dd24c4a90fa7849ea000705052298b65eb6b5b1
Franco RATOVOSON <ratovosonfranco.07@gmail.com>
2020-07-23 14:28:33 +0300

    Update wazo

--- desktop/Wazo/js/main.js
+++ desktop/Wazo/js/main.js
@@ -1,219 +1,454 @@
-/*function wazoStart() {
-    document.addEventListener("DOMContentLoaded", function () {
-        let wazo = new Wazo();
-        wazo.initialize();
-    });
-}*/
+/**
+ * An object that represents a selected concept with its index on the list
+ * @typedef {Object} ConceptReference
+ * @property {Concept} concept
+ * @property {number} index
+ */
 
+/**
+ * An object that represents a selected view in a concept with its index on the list
+ * @typedef {Object} ViewReference
+ * @property {HTMLElement} current.view
+ * @property {number} current.index
+ */
+
+/** The main class.
+ * This wlass will be bind to the "#wazo" element and all the presentations will be managed by here
+ */
 class Wazo {
-    /**
-     *
-     * @param {boolean} [fullscreen=true] If true, the presentation will be set full screen
-     */
     constructor() {
-        this.root = document.getElementsByClassName("wazo")[0];
-
-        this.container = document.getElementsByClassName("container")[0];
-        this.conceptList = document.getElementsByClassName("concept");
-        this.currentItem = { concept: 0, view: 0 };
-        this.currentSizing = { height: 0, width: 0 };
-
-        //Make the element focusable so it can listen to keyboard events
-        this.root.setAttribute("tabindex", "0");
+        //_root setup
+        let root = document.getElementById("wazo");
+        let errorMessage =
+            "Missing #Wazo element.\nPlease add the 'wazo' id to the root element of your choice";
+        if (root != null)
+            /**
+             * Represent the "#wazo" element that will be the root.
+             * @private
+             */
+            this._root = root;
+        else throw new Error(errorMessage);
+        this._root.setAttribute("tabindex", "0");
+        //Container setup
+        let container = this._root.querySelector(".container");
+        errorMessage = "Missing .container element inside roor";
+        if (container != null)
+            /**
+             * Represents the element that will contain all the presentations
+             * @private
+             */
+            this._container = new Container(container);
+        else throw new Error(errorMessage);
     }
 
-    setSizing() {
-        let contents = document.getElementsByClassName("concept__view");
-        let conceptWidth = 100 / this.currentSizing.width;
-        for (let i = 0; i < contents.length; i++)
-            contents[i].style.height = `${100 / this.currentSizing.height}%`;
-        for (let i = 0; i < this.conceptList.length; i++)
-            this.conceptList[i].style.width = `${conceptWidth}%`;
-        this.container.style.width = `${this.currentSizing.width * 100}%`;
-        this.container.style.height = `${this.currentSizing.height * 100}%`;
-    }
-
+    /**
+     * Initialize all the parameters like sizing.
+     * @public
+     */
     initialize() {
-        //Setting up the width of the container
-        this.currentSizing.width = this.conceptList.length;
-        //Setting up the height of the container
-        for (let i = 0; i < this.currentSizing.width; i++) {
-            let conceptSize = selectAllConceptView(this.conceptList[i]).length;
-            if (this.currentSizing.height < conceptSize)
-                this.currentSizing.height = conceptSize;
-        }
-
-        this.root.addEventListener("keyup", this.keyListener);
-        this.setSizing();
+        this._container.initialize();
+        this._root.addEventListener("keydown", this.keyListener);
     }
 
+    /**@private */
     keyListener = (e) => {
-        if (e.key === "ArrowUp" || e.key === "Up") this.up();
-        else if (e.key === "ArrowDown" || e.key === "Down") this.down();
-        else if (e.key === "ArrowLeft" || e.key === "Left") this.left();
-        else if (e.key === "ArrowRight" || e.key === "Right") this.right();
-        else if (e.key === "PageUp") this.prev();
-        else if (e.key === "PageDown") this.next();
+        if (e.key === "ArrowUp" || e.key === "Up") this._container.up();
+        else if (e.key === "ArrowDown" || e.key === "Down")
+            this._container.down();
+        else if (e.key === "ArrowLeft" || e.key === "Left")
+            this._container.left();
+        else if (e.key === "ArrowRight" || e.key === "Right")
+            this._container.right();
+        else if (e.key === "PageUp") this._container.prev();
+        else if (e.key === "PageDown") this._container.next();
     };
+}
 
-    up = () => {
-        if (this.currentItem.view > 0)
-            this.gotoView({
-                concept: this.currentItem.concept,
-                view: this.currentItem.view - 1,
-            });
-    };
+/**
+ * Represents the container of all the presentation.
+ * Will contain each pages (named "concept") and all the views.
+ */
+class Container {
+    /**
+     * @public
+     * @param {ConceptReference} current
+     */
+    set Current(current) {
+        this._current = current;
+    }
+    /**
+     * @public
+     * @returns {ConceptReference}
+     */
+    get Current() {
+        return this._current;
+    }
 
-    down = () => {
-        if (!this.isLastView())
-            this.gotoView({
-                concept: this.currentItem.concept,
-                view: this.currentItem.view + 1,
-            });
-    };
+    /**@returns {boolean} */
+    get atFirst() {
+        return this.Current.index == 0;
+    }
+    /**@returns {boolean} */
+    get atLast() {
+        return this.Current.index == this._conceptList.Length - 1;
+    }
+
+    /** @param {HTMLElement} element */
+    constructor(element) {
+        /** @private */
+        this._element = element;
+        /** @private */
+        this._conceptList = new ConceptList(
+            this._element.getElementsByClassName("concept")
+        );
+        /**
+         * @private
+         * @type {ConceptReference}
+         */
+        this._current = { concept: this._conceptList.FirstElement, index: 0 };
+    }
+
+    /**
+     * Initialize all the parameters like sizing.
+     * @public
+     */
+    initialize() {
+        this._element.style.gridTemplateColumns = `repeat(${this._conceptList.Length},100%)`;
+        this._conceptList.initialize();
+    }
+
+    /**
+     * Make the move on the screen by translating the container.
+     * @private
+     * @param {ConceptReference} target The targeted concept.
+     */
+    _move(target) {
+        if (target.index != this.Current.index) {
+            let decallage = (this.Current.index - target.index) * 100;
+            let currentDecallage = this.Current.index * -100;
+            this._element.style.transform = `translateX(${
+                currentDecallage + decallage
+            }%)`;
+            this.Current = Object.assign({}, target);
+        }
+    }
+
+    up = () => this.Current.concept.prev();
+
+    down = () => this.Current.concept.next();
 
     left = () => {
-        if (this.currentItem.concept > 0)
-            this.gotoView({ concept: this.currentItem.concept - 1, view: 0 });
+        let target = this._conceptList.prev(this.Current.index);
+        this._move(target);
     };
 
     right = () => {
-        if (!this.isLastConcept())
-            this.gotoView({ concept: this.currentItem.concept + 1, view: 0 });
+        let target = this._conceptList.next(this.Current.index);
+        this._move(target);
     };
 
     prev = () => {
-        if (this.currentItem.view === 0 && this.currentItem.concept > 0) {
-            let conceptGoal = this.currentItem.concept - 1;
-            this.gotoView({
-                concept: conceptGoal,
-                view: this.getConceptLength(conceptGoal) - 1,
-            });
+        if (this.Current.concept.atFirst) {
+            if (!this.atFirst) {
+                this.left();
+                this.Current.concept.toLast();
+            }
         } else this.up();
     };
 
     next = () => {
-        if (this.isLastView() && !this.isLastConcept()) this.right();
-        else this.down();
+        if (this.Current.concept.atLast) {
+            if (!this.atLast) {
+                this.right();
+                this.Current.concept.toFirst();
+            }
+        } else this.down();
+    };
+}
+
+/**
+ * A class that represents the list of all concpets(pages)
+ */
+class ConceptList {
+    /**
+     * @returns {Concept}
+     */
+    get FirstElement() {
+        return this.getElement(0);
+    }
+    /**
+     * @returns {Concept}
+     */
+    get LastElement() {
+        return this.getElement(this.Length - 1);
+    }
+    /**
+     * @returns {number}
+     */
+    get Length() {
+        return this._collection.length;
+    }
+
+    /**
+     *
+     * @param {HTMLCollectionOf<Element>} conceptList
+     */
+    constructor(conceptList) {
+        /**
+         * @private
+         * @type {Array<Concept>}
+         */
+        this._collection = [];
+        for (let i = 0; i < conceptList.length; i++) {
+            this._collection.push(new Concept(conceptList[i]));
+        }
+    }
+
+    /**
+     * Initialize each concept.
+     */
+    initialize() {
+        this._collection.forEach((concept) => concept.initialize());
+    }
+
+    /**
+     * @returns {ConceptReference}
+     */
+    toFirst = () => {
+        return { concept: this.FirstElement, index: 0 };
+    };
+    /**
+     * @returns {ConceptReference}
+     */
+    toLast = () => {
+        return { concept: this.LastElement, index: this.Length - 1 };
     };
 
     /**
-     *  Go to specific index
-     * @param {Object} index Index of the target
-     * @param {Object} index.concept index of the target concept (x)
-     * @param {Object} index.view index of the target view on the target concept (y)
+     *
+     * @param {number} index
+     * @returns {ConceptReference}
      */
-    gotoView = (index) => {
-        let translateX = (-100 * index.concept) / this.currentSizing.width;
-        let translateY = (-100 * index.view) / this.currentSizing.height;
-
-        this.container.style.transform = `translateX(${translateX}%)`;
-
-        if (this.didVerticalyMoved(index)) {
-            if (
-                selectAllConceptView(this.conceptList[index.concept]).length > 1
-            )
-                selectAllConceptView(this.conceptList[index.concept])[
-                    this.currentItem.view
-                ].style.animation = "";
-            if (this.didHorizontalyMoved(index))
-                selectAllConceptView(
-                    this.conceptList[this.currentItem.concept]
-                )[this.currentItem.view].style.animation = "";
-        }
-
-        this.conceptList[
-            index.concept
-        ].style.transform = `translateY(${translateY}%)`;
-
-        if (this.didVerticalyMoved(index) && !this.didHorizontalyMoved(index)) {
-            selectAllConceptView(this.conceptList[index.concept])[
-                index.view
-            ].style.animation = "fadein 1.5s";
-        }
-
-        this.currentItem = Object.assign({}, index);
+    prev = (index) => {
+        if (index <= 0) return this.toFirst();
+        else return { concept: this.getElement(index - 1), index: index - 1 };
+    };
+    /**
+     *
+     * @param {number} index
+     * @returns {ConceptReference}
+     */
+    next = (index /*: number*/) /*: { concept: Concept; index: number }*/ => {
+        if (index >= this.Length - 1) return this.toLast();
+        else return { concept: this.getElement(index + 1), index: index + 1 };
     };
 
     /**
-     * Return the state on the current concept.
-     * @returns {boolean}
+     *
+     * @param {number} index
+     * @returns {Concept}
      */
-    isLastView = () => {
-        let conceptViewList = selectAllConceptView(
-            this.conceptList[this.currentItem.concept]
+    getElement(index) /*: Concept */ {
+        return this._collection[index];
+    }
+
+    /**
+     * Get the Maximum size of all the concepts.
+     * @returns {number}
+     */
+    getMaxSize() {
+        let max = 0;
+        this._collection.forEach(
+            (concept) => (max = max < concept.Size ? concept.Size : max)
         );
-        return this.currentItem.view === conceptViewList.length - 1;
-    };
-
-    /**
-     * Return the state on all concept.
-     * @returns {boolean}
-     */
-    isLastConcept = () => {
-        return this.currentItem.concept === this.conceptList.length - 1;
-    };
-    /**
-     * Return the length of the current concept.
-     * @returns {number}
-     */
-    getCurrentConceptLength = () => {
-        return this.getConceptLength(this.currentItem.concept);
-    };
-    /**
-     * Return the length of the concept send to the "conceptIndex" param
-     * @param {number} conceptIndex
-     * @returns {number}
-     */
-    getConceptLength = (conceptIndex) => {
-        return conceptIndex >= 0 && conceptIndex < this.conceptList.length
-            ? selectAllConceptView(this.conceptList[conceptIndex]).length
-            : 0;
-    };
-
-    /**
-     *
-     * @param {object} index
-     * @param {object} index.concept
-     * @param {object} index.view
-     * @returns {boolean}
-     */
-    didHorizontalyMoved(index) {
-        return index.concept !== this.currentItem.concept;
-    }
-
-    /**
-     *
-     * @param {object} index
-     * @param {object} index.concept
-     * @param {object} index.view
-     * @returns {boolean}
-     */
-    didVerticalyMoved(index) {
-        return index.view !== this.currentItem.view;
-    }
-
-    /**
-     *
-     * @param {object} index
-     * @param {object} index.concept
-     * @param {object} index.view
-     * @returns {boolean}
-     */
-    didMoved(index) {
-        return this.didHorizontalyMoved(index) || this.didVerticalyMoved(index);
+        return max;
     }
 }
 
-//******/
-/*UTILS*/
-//******/
+/**
+ * Represents a page of the presentation (concept) with its all view.
+ */
+class Concept {
+    /**
+     * @public
+     * @param {ViewReference} current
+     */
+    set Current(current) {
+        this._current = current;
+    }
+    /**
+     * @public
+     * @returns {ViewReference}
+     */
+    get Current() {
+        return this._current;
+    }
+
+    /**
+     * The size of the current concept (count of its view)
+     * @returns {number}
+     */
+    get Size() {
+        return this._viewList.Length;
+    }
+    /**
+     * Check if the current view if the first view of the concept
+     * @returns {boolean}
+     */
+    get atFirst() {
+        return this.Current.index == 0;
+    }
+    /**
+     * Check if the current view is the last of the concept
+     * @returns {boolean}
+     */
+    get atLast() {
+        return this.Current.index == this._viewList.Length - 1;
+    }
+
+    /**
+     *
+     * @param {Element} element
+     */
+    constructor(element) {
+        /**
+         * @private
+         * @type {Element}
+         */
+        this._element = element;
+        /**
+         * @private
+         * @type {ViewList}
+         */
+        this._viewList = new ViewList(
+            this._element.getElementsByClassName("concept__view")
+        );
+        /**
+         * @private
+         * @type {ViewReference}
+         */
+        this._current = { view: this._viewList.FirstElement, index: 0 };
+    }
+
+    /**
+     * Initialize all the views in the view list.
+     */
+    initialize() {
+        this._viewList.initialize();
+    }
+
+    /**
+     *
+     * @param {ViewReference} target
+     * @param {boolean} animated
+     */
+    _move(target, animated) {
+        if (target.index != this.Current.index) {
+            target.view.style.transition = animated ? "opacity 1s" : "";
+            this.Current.view.style.transition = animated ? "opacity 0.5s" : "";
+            target.view.style.opacity = "1";
+            this.Current.view.style.opacity = "0";
+
+            this.Current = Object.assign({}, target);
+        }
+    }
+
+    prev = () => {
+        let target = this._viewList.prev(this.Current.index);
+        this._move(target, true);
+    };
+
+    next = () => {
+        let target = this._viewList.next(this.Current.index);
+        this._move(target, true);
+    };
+
+    toLast = () => {
+        let target = this._viewList.toLast();
+        this._move(target, false);
+    };
+
+    toFirst = () => {
+        let target = this._viewList.toFirst();
+        this._move(target, false);
+    };
+}
 
 /**
- *  Get all ".concept__view" in the concept send to "element"
- * @param {HTMLElement|Document} element
- * @returns {NodeListOf<Element>}
+ * Represents the list of all views inside a concept
  */
-function selectAllConceptView(element) {
-    return element.querySelectorAll(".concept__view");
+class ViewList {
+    /**
+     * @returns {HTMLElement}
+     */
+    get FirstElement() {
+        return this.getElement(0);
+    }
+    /**
+     * @returns {HTMLElement}
+     */
+    get LastElement() {
+        return this.getElement(this.Length - 1);
+    }
+    /**
+     * @returns {number}
+     */
+    get Length() {
+        return this._collection.length;
+    }
+
+    /**
+     *
+     * @param {HTMLCollectionOf<Element>} viewList
+     */
+    constructor(viewList) {
+        /**
+         * @type {HTMLCollectionOf<HTMLElement>}
+         * @private
+         */
+        this._collection = viewList;
+    }
+
+    /**
+     * Initialize the opacity off all view to let only the top(first) view to be visible
+     */
+    initialize() {
+        for (let i = 1; i < this._collection.length; i++) {
+            let view = this._collection[i];
+            view.style.opacity = "0";
+        }
+    }
+
+    toFirst = () => {
+        return { view: this.FirstElement, index: 0 };
+    };
+
+    toLast = () => {
+        return { view: this.LastElement, index: this.Length - 1 };
+    };
+
+    prev = (index) => {
+        if (index <= 0) return this.toFirst();
+        else return { view: this.getElement(index - 1), index: index - 1 };
+    };
+
+    next = (index) => {
+        if (index >= this.Length - 1) return this.toLast();
+        else return { view: this.getElement(index + 1), index: index + 1 };
+    };
+
+    /**
+     * Get a specific element by its index
+     * @param {number} index
+     * @returns {HTMLElement}
+     */
+    getElement(index) {
+        return this._collection[index];
+    }
+}
+
+class View extends Element {
+    constructor() {
+        super();
+    }
 }
